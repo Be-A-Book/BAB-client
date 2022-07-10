80cf2558faa34d5172cf10eb27bbcd3d60ec7cc0
won0324 <huiwon0324@naver.com>
2022-07-10 17:25:01 +0900

    Map 페이지에 KakaoMap API 적용

--- bab-client/src/pages/Map.js
+++ bab-client/src/pages/Map.js
@@ -1,28 +1,54 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../css/Map.css";
+const { kakao } = window;
 
 const Map = () => {
-    const [bookStore, setBookStore] = useState('');
+  const [bookStore, setBookStore] = useState("");
 
-    const getDetails = (data) => { //BookStore는 검색어... 같은 느낌이야, 전체를 다 불러올 때는 필요가 없어
-        axios.get(`API_ENDPOINT + '/bookstore/${data}`).then((response) => {  //ENDPOINT 포트이름 변수 + 내가 요청할 주소
-            if(response.data.success) {
-                setBookStore(response.data);
-                console.log("불러오기");
-            } else {
-                console.log("불러오기 실패");
-            }
-        });
-    }
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(37.555206675339164, 126.93689191198305),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
 
-    return(
-        <>
-            <div>
-            {bookStore.name}
-            지도 페이지
-            </div>
-        </>
-    )
-}
+    //마커가 표시 될 위치 - 이 부분은 서버 모델과 연결하는 것으로 변경
+    const markerPosition = new kakao.maps.LatLng(
+      37.55579240200719,
+      126.9370272266829
+    );
+
+    // 마커 생성
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 지도 위에 마커 표시
+    marker.setMap(map);
+  }, []);
+
+  const getDetails = (data) => {
+    //BookStore는 검색어... 같은 느낌이야, 전체를 다 불러올 때는 필요가 없어
+    axios.get(`API_ENDPOINT + '/bookstore/${data}`).then((response) => {
+      //ENDPOINT 포트이름 변수 + 내가 요청할 주소
+      if (response.data.success) {
+        setBookStore(response.data);
+        console.log("불러오기");
+      } else {
+        console.log("불러오기 실패");
+      }
+    });
+  };
+
+  return (
+    <>
+      <div id="map" className="Map">
+        {bookStore.name}
+      </div>
+    </>
+  );
+};
 
 export default Map;
