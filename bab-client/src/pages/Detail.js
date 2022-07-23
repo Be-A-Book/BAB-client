c96ec109519c83974f5de510cd6ef6cd53a31def
kimpeep <rnunseoo@gmail.com>
2022-07-24 03:42:43 +0900

    detail axios로 데이터 받아오기

--- bab-client/src/pages/Detail.js
+++ bab-client/src/pages/Detail.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
 import "../css/Detail.css"
 import axios from 'axios';
 import logo from "../img/logo_wax2.png";
@@ -7,12 +6,14 @@ import image from "../img/bab_black.png";
 import heart from "../img/heart.png";
 
 const Detail = () => {    
+
     const [bookStore, setBookStore] = useState('');
+    const [review, setReview] = useState('');
 
     useEffect (() => { 
         axios({
             method:"post",
-            url:`/bookstore/getBookstoreDetail`,
+            url:`/api/bookstore/getBookstoreDetail`,
             data: {
                 "_id": "62d6935ed5649a098281cf65", //storekey 임의 지정
             }
@@ -22,51 +23,78 @@ const Detail = () => {
                 setBookStore(response.data);
                 console.log("불러오기");
                 console.log(response.data)
+                
             } else {
                 console.log("불러오기 실패");
             }
         });
     }, []);
 
+    useEffect (() => { 
+        axios({
+            method:"get",
+            url:`/api/review/getReviews/62c926a80ea12db83c87b5e9`,
+            
+        })
+        .then((response) => {  
+            if(response.data.success) {
+                setReview(response.data);
+                console.log("불러오기");
+                console.log(response.data)
+                
+            } else {
+                console.log("불러오기 실패");
+            }
+        });
+    }, []);
+    
     return(
         <>
         <div className="detail">
             {/* 세부 내용 좌측 */}
             <div className="detail-content-left">
                 <div className="detail-big-title">
-                    제목
+                    { bookStore && bookStore.bookstore.name}
                 </div>
                 <div className="detail-container-2">
                     <div className="detail-small-title-column">
                         소개
                     </div>
                     <div className="detail-text">
-                        어쩌구 저쩌구 ~~~~
+                        { bookStore && bookStore.bookstore.introduction} 
                     </div>
                 </div>
                 <div className="detail-container">
-                    <view className="detail-small-title-row">
+                    <div className="detail-small-title-row">
                         주소
-                    </view>
-                    <view className="detail-text">
-                        서울특별시 00구 00동~~
-                    </view>
+                    </div>
+                    <div className="detail-text">
+                        { bookStore && bookStore.bookstore.address}
+                    </div>
                 </div>
                 <div className="detail-container">
-                    <view className="detail-small-title-row">
+                    <div className="detail-small-title-row">
                         운영시간
-                    </view>
-                    <view className="detail-text">
-                        월-토 00:00~00:00
-                    </view>
+                    </div>
+                    <div className="detail-text">
+                        { bookStore && bookStore.bookstore.operation}
+                    </div>
                 </div>
                 <div className="detail-container">
-                    <view className="detail-small-title-row">
+                    <div className="detail-small-title-row">
                         휴무일
-                    </view>
-                    <view className="detail-text">
-                        일요일
-                    </view>
+                    </div>
+                    <div className="detail-text">
+                        { bookStore && bookStore.bookstore.holiday}
+                    </div>
+                </div>
+                <div className="detail-container">
+                    <div className="detail-small-title-row">
+                        웹사이트
+                    </div>
+                    <div className="detail-text">
+                        { bookStore && bookStore.bookstore.website}
+                    </div>
                 </div>
             </div>
             {/* 세부 내용 우측 */}
@@ -77,30 +105,43 @@ const Detail = () => {
                     7
                 </div>
                 <div className="image">
-                    <view className="image-image">
-                    <img alt="서점 이미지" src={image} width="360px" height="360px"/>
-                    </view>
+                    <div className="image-image">
+                    <img alt="서점 이미지" src={image} width="360px" height="360px"/> {/* image 불러오기 불가능, 임의 수정 bookStore && bookStore.bookstore.defaultImage */}
+                    </div>
                     <div className="image-text">
-                        #아늑한 #조용한 #헌책방
+                    { bookStore && bookStore.bookstore.tags[0].name} {/* array 개수 받아와서 tags에 임의 수정 필요 */}
                     </div>
                 </div>
             </div>
             {/* 방문 후기 */}
             <div className="review">
-                <view className="detail-review">
+                <div className="detail-review">
                     방문후기
-                </view>
+                </div>
                 <div className="review-card">
                     <div className="review-card-image">
                         <img alt="프로필 아이콘" src={logo} width="60px" height="60px"/>
                     </div>
                     <div className="review-card-content">
-                        <view className="review-card-top">
-                            작성자
-                        </view>
-                        <view className="review-card-bottom">
-                            작성자 후기~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                        </view>
+                        <div className="review-card-top">
+                        { review && review.reviews[0].writer.name}
+                        </div>
+                        <div className="review-card-bottom">
+                        { review && review.reviews[0].content}
+                        </div>
+                    </div>
+                </div>
+                <div className="review-card">
+                    <div className="review-card-image">
+                        <img alt="프로필 아이콘" src={logo} width="60px" height="60px"/>
+                    </div>
+                    <div className="review-card-content">
+                        <div className="review-card-top">
+                        { review && review.reviews[1].writer.name}
+                        </div>
+                        <div className="review-card-bottom">
+                        { review && review.reviews[1].content}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ bab-client/src/pages/ReviewWrite.js
@@ -0,0 +1,2 @@
+import React, { useState, useEffect } from "react";
+import "../css/ReviewWrite.css";
