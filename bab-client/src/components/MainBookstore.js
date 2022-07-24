8ffdad92e3deeaf3f3dd1532758b93eed78dfa3b
kimpeep <rnunseoo@gmail.com>
2022-07-24 20:43:30 +0900

    main 페이지 data 연결

--- bab-client/src/components/MainBookstore.js
+++ bab-client/src/components/MainBookstore.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../css/MainBookstore.css";
 import image from "../img/bab_black.png";
 import heart from "../img/heart.png";
 import stamp from "../img/stamp.png";
+import axios from 'axios';
 
 const MainBookstore = () => {
+    const [bookStore, setBookStore] = useState('');
+
+    useEffect (() => { 
+        axios({
+            method:"post",
+            url:`/api/bookstore/getBookstoreDetail`,
+            data: {
+                "_id": "62d6935ed5649a098281cf65", //storekey 임의 지정
+            }
+        })
+        .then((response) => {  
+            if(response.data.success) {
+                setBookStore(response.data);
+                // console.log("불러오기");
+                console.log(response.data)
+                console.log(response.data.bookstore.tags)
+                
+            } else {
+                // console.log("불러오기 실패");
+            }
+        });
+    }, []);    
     return (
     <>
         <div className="MainBookstore">
             <div className="main-book-hashtag">
-                #아늑한 #조용한 #헌책방
+                { bookStore && bookStore.bookstore.tags.map ((tags =>
+                        <div className="image-text-hash" key={tags._id}> #{tags && tags.name } </div>
+                ))}
             </div>
             <div className="main-bookarea">
                 <div className="main-book-image">
@@ -21,7 +46,7 @@ const MainBookstore = () => {
                             소개
                         </div>
                         <div className="main-book-text">
-                            어쩌구 저쩌구 ~~~~
+                        { bookStore && bookStore.bookstore.introduction}
                         </div>
                     </div>
                     <div className="main-book-container">
@@ -29,7 +54,7 @@ const MainBookstore = () => {
                             주소
                         </div>
                         <div className="main-book-text">
-                            서울특별시 00구 00동~~
+                        { bookStore && bookStore.bookstore.address}
                         </div>
                     </div>
                 </div>
@@ -37,7 +62,7 @@ const MainBookstore = () => {
                     <div className="main-book-stamp">
                         <img alt="스탬프 버튼" src={stamp} width="300px"/>
                         <div className="main-book-stamp-text">
-                            00서점
+                        { bookStore && bookStore.bookstore.name}
                         </div>
                         
                     </div>
--- bab-client/src/pages/ReviewWrite.js
+++ bab-client/src/pages/ReviewWrite.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "../css/ReviewWrite.css";
-import writeBook from "../img/write_book.png";
 
 
 const ReviewWrite = () => {
