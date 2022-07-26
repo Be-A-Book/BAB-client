15db6eca086775f63fea5108b9f144b3614d8cb3
won0324 <huiwon0324@naver.com>
2022-07-27 00:04:07 +0900

    무시

--- bab-client/src/components/MainBookstore.js
+++ bab-client/src/components/MainBookstore.js
@@ -1,80 +1,77 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../css/MainBookstore.css";
 import image from "../img/bab_black.png";
 import heart from "../img/heart.png";
 import stamp from "../img/stamp.png";
-import axios from 'axios';
+import axios from "axios";
 
 const MainBookstore = () => {
-    const [bookStore, setBookStore] = useState('');
+  const [bookStore, setBookStore] = useState("");
 
-    useEffect (() => { 
-        axios({
-            method:"post",
-            url:`/api/bookstore/getBookstoreDetail`,
-            data: {
-                "_id": "62d6935ed5649a098281cf65", //storekey 임의 지정
-            }
-        })
-        .then((response) => {  
-            if(response.data.success) {
-                setBookStore(response.data);
-                // console.log("불러오기");
-                console.log(response.data)
-                console.log(response.data.bookstore.tags)
-                
-            } else {
-                // console.log("불러오기 실패");
-            }
-        });
-    }, []);    
-    return (
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: `/api/bookstore/getBookstoreDetail`,
+      data: {
+        _id: "62dfde11ce45d9283ff4e8d8", //storekey 임의 지정
+      },
+    }).then((response) => {
+      if (response.data.success) {
+        setBookStore(response.data);
+        // console.log("불러오기");
+        console.log(response.data);
+        console.log(response.data.bookstore.tags);
+      } else {
+        // console.log("불러오기 실패");
+      }
+    });
+  }, []);
+  return (
     <>
-        <div className="MainBookstore">
-            <div className="main-book-hashtag">
-                { bookStore && bookStore.bookstore.tags.map ((tags =>
-                        <div className="image-text-hash" key={tags._id}> #{tags && tags.name } </div>
-                ))}
-            </div>
-            <div className="main-bookarea">
-                <div className="main-book-image">
-                    <img alt="서점 이미지" src={image} width="360px" height="360px"/>
-                </div>
-                <div className="main-book-textarea">
-                    <div className="main-book-container">
-                        <div className="main-book-small-title-row">
-                            소개
-                        </div>
-                        <div className="main-book-text">
-                        { bookStore && bookStore.bookstore.introduction}
-                        </div>
-                    </div>
-                    <div className="main-book-container">
-                        <div className="main-book-small-title-row">
-                            주소
-                        </div>
-                        <div className="main-book-text">
-                        { bookStore && bookStore.bookstore.address}
-                        </div>
-                    </div>
-                </div>
-                <div className="main-book-contentarea">
-                    <div className="main-book-stamp">
-                        <img alt="스탬프 버튼" src={stamp} width="300px"/>
-                        <div className="main-book-stamp-text">
-                        { bookStore && bookStore.bookstore.name}
-                        </div>
-                        
-                    </div>
-                    <div className="main-book-heart">
-                        <img alt="하트 버튼" src={heart} width="30px" height="30px"/> {/*인프런 한번 더 확인*/}
-                        7
-                    </div>
-                </div>
-            </div>
+      <div className="MainBookstore">
+        <div className="main-book-hashtag">
+          {bookStore &&
+            bookStore.bookstore.tags.map((tags) => (
+              <div className="image-text-hash" key={tags._id}>
+                {" "}
+                #{tags && tags.name}{" "}
+              </div>
+            ))}
         </div>
+        <div className="main-bookarea">
+          <div className="main-book-image">
+            <img alt="서점 이미지" src={image} width="360px" height="360px" />
+          </div>
+          <div className="main-book-textarea">
+            <div className="main-book-container">
+              <div className="main-book-small-title-row">소개</div>
+              <div className="main-book-text">
+                {bookStore && bookStore.bookstore.introduction}
+              </div>
+            </div>
+            <div className="main-book-container">
+              <div className="main-book-small-title-row">주소</div>
+              <div className="main-book-text">
+                {bookStore && bookStore.bookstore.address}
+              </div>
+            </div>
+          </div>
+          <div className="main-book-contentarea">
+            <div className="main-book-stamp">
+              <img alt="스탬프 버튼" src={stamp} width="300px" />
+              <div className="main-book-stamp-text">
+                {bookStore && bookStore.bookstore.name}
+              </div>
+            </div>
+            <div className="main-book-heart">
+              <img alt="하트 버튼" src={heart} width="30px" height="30px" />{" "}
+              {/*인프런 한번 더 확인*/}7
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-    );
+  );
 };
 
 export default MainBookstore;
--- bab-client/src/pages/Detail.js
+++ bab-client/src/pages/Detail.js
@@ -1,109 +1,106 @@
-import React, {useState, useEffect} from 'react';
-import "../css/Detail.css"
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "../css/Detail.css";
+import axios from "axios";
 // import image from "../img/bab_black.png";
 import heart from "../img/heart.png";
-import DetailReview from '../components/DetailReview';
+import DetailReview from "../components/DetailReview";
 
-const Detail = () => {    
-    const [bookStore, setBookStore] = useState('');
+const Detail = () => {
+  const [bookStore, setBookStore] = useState("");
 
-    useEffect (() => { 
-        axios({
-            method:"post",
-            url:`/api/bookstore/getBookstoreDetail`,
-            data: {
-                "_id": "62d6935ed5649a098281cf65", //storekey 임의 지정
-            }
-        })
-        .then((response) => {  
-            if(response.data.success) {
-                setBookStore(response.data);
-                // console.log("불러오기");
-                // console.log(response.data)
-                // console.log(response.data.bookstore.tags)
-            } else {
-                // console.log("불러오기 실패");
-            }
-        });
-    }, []);    
-    return(
-        <>
-        <div className="detail">
-            {/* 세부 내용 좌측 */}
-            <div className="detail-content-left">
-                <div className="detail-big-title">
-                    { bookStore && bookStore.bookstore.name}
-                </div>
-                <div className="detail-container-2">
-                    <div className="detail-small-title-column">
-                        소개
-                    </div>
-                    <div className="detail-text">
-                        { bookStore && bookStore.bookstore.introduction} 
-                    </div>
-                </div>
-                <div className="detail-container">
-                    <div className="detail-small-title-row">
-                        주소
-                    </div>
-                    <div className="detail-text">
-                        { bookStore && bookStore.bookstore.address}
-                    </div>
-                </div>
-                <div className="detail-container">
-                    <div className="detail-small-title-row">
-                        운영시간
-                    </div>
-                    <div className="detail-text">
-                        { bookStore && bookStore.bookstore.operation}
-                    </div>
-                </div>
-                <div className="detail-container">
-                    <div className="detail-small-title-row">
-                        휴무일
-                    </div>
-                    <div className="detail-text">
-                        { bookStore && bookStore.bookstore.holiday}
-                    </div>
-                </div>
-                <div className="detail-container">
-                    <div className="detail-small-title-row">
-                        웹사이트
-                    </div>
-                    <div className="detail-text">
-                        { bookStore && bookStore.bookstore.website}
-                    </div>
-                </div>
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: `/api/bookstore/getBookstoreDetail`,
+      data: {
+        _id: "62dfde11ce45d9283ff4e8d8", //storekey 임의 지정
+      },
+    }).then((response) => {
+      if (response.data.success) {
+        setBookStore(response.data);
+        // console.log("불러오기");
+        // console.log(response.data)
+        // console.log(response.data.bookstore.tags)
+      } else {
+        // console.log("불러오기 실패");
+      }
+    });
+  }, []);
+  return (
+    <>
+      <div className="detail">
+        {/* 세부 내용 좌측 */}
+        <div className="detail-content-left">
+          <div className="detail-big-title">
+            {bookStore && bookStore.bookstore.name}
+          </div>
+          <div className="detail-container-2">
+            <div className="detail-small-title-column">소개</div>
+            <div className="detail-text">
+              {bookStore && bookStore.bookstore.introduction}
             </div>
-            {/* 세부 내용 우측 */}
-            
-            <div className="detail-content-right">
-                <div className="heart">
-                    <img alt="하트 버튼" src={heart} width="30px" height="30px"/> {/*인프런 한번 더 확인*/}
-                    7
-                </div>
-                <div className="image">
-                    <div className="image-image">
-                    <img alt={bookStore && bookStore.bookstore.defaultImage} src={bookStore && bookStore.bookstore.defaultImage} width="360px" height="360px"/> {/* image 불러오기 불가능, 임의 수정 bookStore && bookStore.bookstore.defaultImage */}
-                    </div>
-                    <div className="image-text">
-                    { bookStore && bookStore.bookstore.tags.map ((tags =>
-                        <div className="image-text-hash" key={tags._id}> #{tags && tags.name } </div>
-                    ))}
-                    </div>
-                </div>
+          </div>
+          <div className="detail-container">
+            <div className="detail-small-title-row">주소</div>
+            <div className="detail-text">
+              {bookStore && bookStore.bookstore.address}
             </div>
-            {/* 방문 후기 */}
-            <div className="review">
-                <div className="detail-review">
-                    방문후기
-                </div>
-                <DetailReview />
+          </div>
+          <div className="detail-container">
+            <div className="detail-small-title-row">운영시간</div>
+            <div className="detail-text">
+              {bookStore && bookStore.bookstore.operation}
             </div>
+          </div>
+          <div className="detail-container">
+            <div className="detail-small-title-row">휴무일</div>
+            <div className="detail-text">
+              {bookStore && bookStore.bookstore.holiday}
+            </div>
+          </div>
+          <div className="detail-container">
+            <div className="detail-small-title-row">웹사이트</div>
+            <div className="detail-text">
+              {bookStore && bookStore.bookstore.website}
+            </div>
+          </div>
         </div>
-        </>
-    )
-}
+        {/* 세부 내용 우측 */}
+
+        <div className="detail-content-right">
+          <div className="heart">
+            <img alt="하트 버튼" src={heart} width="30px" height="30px" />{" "}
+            {/*인프런 한번 더 확인*/}7
+          </div>
+          <div className="image">
+            <div className="image-image">
+              <img
+                alt={bookStore && bookStore.bookstore.defaultImage}
+                src={bookStore && bookStore.bookstore.defaultImage}
+                width="360px"
+                height="360px"
+              />{" "}
+              {/* image 불러오기 불가능, 임의 수정 bookStore && bookStore.bookstore.defaultImage */}
+            </div>
+            <div className="image-text">
+              {bookStore &&
+                bookStore.bookstore.tags.map((tags) => (
+                  <div className="image-text-hash" key={tags._id}>
+                    {" "}
+                    #{tags && tags.name}{" "}
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        {/* 방문 후기 */}
+        <div className="review">
+          <div className="detail-review">방문후기</div>
+          <DetailReview />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Detail;
