79d776b1c8073def4c4492e4b3630c18a85eb1cc
kimpeep <rnunseoo@gmail.com>
2022-07-29 04:49:04 +0900

    로그아웃 기능 구현

--- bab-client/src/pages/Logout.js
+++ bab-client/src/pages/Logout.js
@@ -1,8 +1,20 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 import "../css/Logout.css";
 import logo from "../img/logobab.png";
+import {setToken} from "../redux/reducers/AuthReducer";
 
 const Logout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onclick = async() => {
+
+    await dispatch(setToken(""));
+    console.log("로그아웃되었습니다.");
+    navigate("/")
+  }
   return (
     <>
       <div className="Logout">
@@ -16,7 +28,7 @@ const Logout = () => {
           height="60px"
         />
         <div className="logout-button">
-          <button className="logout-yes-button">Yes</button>
+          <button className="logout-yes-button" onClick={onclick}>Yes</button>
           <button className="logout-no-button">No</button>
         </div>
       </div>
