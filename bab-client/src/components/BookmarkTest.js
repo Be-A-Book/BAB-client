a450232b5dbe44ba32cd923417fdef96df18dab9
kimpeep <rnunseoo@gmail.com>
2022-08-05 05:35:24 +0900

    책갈피 테스트 결과 페이지 제작 완료

--- bab-client/src/components/BookmarkTest.js
+++ bab-client/src/components/BookmarkTest.js
@@ -2,27 +2,31 @@ import React, { useState } from 'react';
 import "../css/BookmarkTest.css"
 import BookmarkTestResult from './BookmarkTestResult';
 
+
 const BookmarkTest = () => {
 
     const [tstVisible, setTstVisible] = useState(true);
     const [rstVisible, setRstVisible] = useState(false);
-    const testChange = () => {
+    const [number, setNumber] = useState(0);
+    const testChange = (props) => {
         setTstVisible(!tstVisible);
         setRstVisible(!rstVisible);
+        setNumber(props)
     }
 
     return (
         <>
+
         <div className={`bookmarkSelect ${ tstVisible ? '' : 'hidden'}`}>
-            <button className="bookmarkChoice1" onClick={ () => { testChange() }} /> 
-            <button className="bookmarkChoice2" onClick={ () => { testChange() }} /> 
-            <button className="bookmarkChoice3" onClick={ () => { testChange() }} /> 
-            <button className="bookmarkChoice4" onClick={ () => { testChange() }} /> 
-            <button className="bookmarkChoice5" onClick={ () => { testChange() }} /> 
-            <button className="bookmarkChoice6" onClick={ () => { testChange() }} /> 
+            <button className="bookmarkChoice1" onClick={ () => { testChange(0) }} /> 
+            <button className="bookmarkChoice2" onClick={ () => { testChange(1) }} /> 
+            <button className="bookmarkChoice3" onClick={ () => { testChange(2) }} /> 
+            <button className="bookmarkChoice4" onClick={ () => { testChange(3) }} /> 
+            <button className="bookmarkChoice5" onClick={ () => { testChange(4) }} /> 
+            <button className="bookmarkChoice6" onClick={ () => { testChange(5) }} /> 
         </div>
         <div>
-            {rstVisible && <BookmarkTestResult /> } 
+            {rstVisible && <BookmarkTestResult number={number} /> } 
         </div>
         </>
     )
--- bab-client/src/components/BookmarkTestResult.js
+++ bab-client/src/components/BookmarkTestResult.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import "../css/BookmarkTestResult.css"
+import bookmarkData from "../data/bookmarkData.json";
 
-const BookmarkTestResult = () => {
+const BookmarkTestResult = (props) => {
+    console.log(props)
+    const data = bookmarkData[props.number]
+    console.log(data)
     return (
         <>
         <div className='bookmarkResult'>
             <div className = "myBookmark"> 
-                <div className = "bookmarkChoice1" />
+                <div className = {`bookmarkChoice${props.number+1}`}/>
                 <div className='bookmarkResultText'>
-                지금, 여기서, 행복할 것!<br />
-                <br />
-                늘 행복과 사랑을 품고 사는<br />
-                열정 넘치는 장미와 같은 사람이군요!<br />
-                <br />
-                이 서점에 당신만의 향기를<br />
-                가득 채워 주시길 바라요.
+                    <br /> 
+                    {data.content1} <br /> <br />
+                    {data.content2} <br />
+                    {data.content3} <br /> <br />
+                    {data.content4} <br />
+                    {data.content5}
                 </div>
             </div>
         </div>
