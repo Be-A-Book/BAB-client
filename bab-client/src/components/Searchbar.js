b0f5c4fb8dda2e0fd3a706c3ac029e1e898b08f9
eunseo <eunseo@eunseoui-MacBookAir.local>
2022-08-05 16:11:26 +0900

    검색 기능 get 추가 ; 현재 CORS error

--- bab-client/src/components/Searchbar.js
+++ bab-client/src/components/Searchbar.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/Searchbar.css';
-import search from '../img/search.png'
+import search from '../img/search.png';
+import axios from "axios";
 
-const Searchbar = ({value, onChange, onCreate, onKeyPress}) => {
+const Searchbar = () => {
+    const [searchValue, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
+
+    const onSearch = (e) => {
+            const response = axios.get(`/api/bookstore+query/search?keyword=${searchValue}`);
+            console.log(response)
+    }
+
+
 return (
     <div className="form">
         <div className="searchbar">
             <img src={search} alt="검색 돋보기" width="40px" height="40px" style={{paddingLeft: "20px", paddingRight: "20px"}} />
-            <input placeholder="검색어를 입력하세요" value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+            <input placeholder="검색어를 입력하세요" value={searchValue} onChange={onChangeSearch}/>
         </div>
-        <div className="create-button" onClick={onCreate}>
+        <div className="create-button" onClick={onSearch}>
             검색
         </div>
     </div>
