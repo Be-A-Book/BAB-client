892fd28e5e81c0c0d47cea4aae381242c6246d2d
eunseo <rnunseoo@gmail.com>
2022-08-12 01:57:43 +0900

    Guestbook 타협

--- bab-client/src/components/GuestbookMemo.js
+++ bab-client/src/components/GuestbookMemo.js
@@ -3,19 +3,6 @@ import React, { useEffect, useState } from 'react';
 import '../css/GuestbookMemo.css'
 
 const GuestBookMemo = (data) => {
-    const [bookmark, setBookmark] = useState();
-    useEffect(()=> {
-        axios({
-            method: "post",
-            url: `/api/users/getBookmark`,
-            data: {
-                email: `${data.data.writer && data.data.writer.email}`,
-            }
-        }).then((response) => {
-            setBookmark(response.bookmark);
-        });
-    });
-
     return(
         <div className="guestbookmemo">
             <div className='guestbookmemoWriter'>
@@ -23,16 +10,6 @@ const GuestBookMemo = (data) => {
             </div>
             <div>
             {data.data && data.data.message}
-            </div>
-            <div>
-                {bookmark && (
-                <div
-                    className="guestbookBookmark"
-                    style={{
-                    backgroundColor: bookmark.color,
-                    }}
-                >bookmark</div>
-                )}
             </div>
         </div>
     )
