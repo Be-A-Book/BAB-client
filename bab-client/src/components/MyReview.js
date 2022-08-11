ecaaebae86406b31949ea4194ec3a1e3f04158fc
eunseo <rnunseoo@gmail.com>
2022-08-12 03:16:35 +0900

    My Review Mapping

--- bab-client/src/components/MyReview.js
+++ bab-client/src/components/MyReview.js
@@ -4,31 +4,22 @@ import "../css/MyReview.css";
 
 const MyReview = ({ data }) => {
   const [bookStore, setBookstore] = useState("");
+  const [bookStoreId, setBookstoreId] = useState("");
   const [datas, setData] = useState("");
-  const [state, setState] = useState(false)
+  const [state, setState] = useState(false);
+  const [id, setId] = useState();
 
   useEffect(()=> {
     if (data.length === 0) {
       setData("작성한 리뷰 없음")
       setState(false)
     } else {
+      setData(data)
       setState(true)
-      new useEffect(() => {
-        axios({
-          method: "post",
-          url: `/api/bookstore/getBookstoreDetail`,
-          data: {
-            _id: data[0].store,
-          },
-        }).then((response) => {
-          if (response.data.success) {
-            setBookstore(response.data);
-          } else {
-          }
-        });
-      }, []);
     }
   })  
+
+
 
   function ReviewNo() {
     return(
@@ -39,13 +30,19 @@ const MyReview = ({ data }) => {
   }
 
   function ReviewYes() {
+
     return(
       <>
+      {datas && datas.map((Review) => (
+      <>
       <div className="my-review-bookstore">
-      {datas && datas}  
+        {Review && Review.store?.name}
       </div>
-      <div className="my-review-content">{data[0].content}
+      <div className="my-review-content">{Review && Review.content}
       </div> 
+      </>
+      ))}
+
     </>
     )
   }
