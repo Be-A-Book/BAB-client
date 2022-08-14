182cbd5a54969e19527a4763e1b97a5edf5400b3
kimpeep <rnunseoo@gmail.com>
2022-08-15 01:31:58 +0900

    logout layout

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -7,12 +7,8 @@ import logo from "../img/logobab.png";
 import { setToken } from "../redux/reducers/AuthReducer";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useCookies } from "react-cookie";
 
 const Logout = () => {
-  const COOKIE_KEY = window.LOGIN_KEY; // 상수화시킨 쿠키 값을 넣어줬다.
-
-  const [, , removeCookie] = useCookies([COOKIE_KEY]); // 쓰지 않는 변수는 (공백),처리해주고 removeCookie 옵션만 사용한다
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -29,7 +25,6 @@ const Logout = () => {
       method: "get",
       url: "https://beabook-server.herokuapp.com/api/users/logout",
     });
-    removeCookie("x_auth"); // 쿠키삭제후
     await dispatch(setToken(""));
     console.log();
     console.log("로그아웃되었습니다.");
@@ -44,13 +39,7 @@ const Logout = () => {
         <ToastContainer />
         <div className="logout-title">Log Out</div>
         <div className="logout-text">Are you sure you want to log out?</div>
-        <img
-          className="bab-logo"
-          alt="Be A Book 로고"
-          src={logo}
-          width="60px"
-          height="60px"
-        />
+
         <div className="logout-button">
           <button className="logout-yes-button" onClick={onclick}>
             Yes
